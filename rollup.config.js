528c7874d90c5a9e31ddfaf6aeb87722db9fed66
Sindre Bøyum <sindre.boyum@gmail.com>
2021-03-04 16:26:20 +0100

    Add type definitions to npm package

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,20 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
-  output: {
-    dir: "dist",
-    format: "cjs",
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist",
+      format: "cjs",
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
-};
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/markdown-it-image-size.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
