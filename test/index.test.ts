efcbb3e91c3f883c020ea5a08c3dd46c9bdc48a8
Sindre Bøyum <boyum@users.noreply.github.com>
2023-03-24 17:18:40 +0100

    Support images with no explicit protocol (#400)

--- test/index.test.ts
+++ test/index.test.ts
@@ -48,4 +48,20 @@ describe(markdownItImageSize.name, () => {
 
     expect(actual).toBe(expected);
   });
+
+  it("should render external images with no explicit protocol with attributes for width and height", () => {
+    const markdownRenderer = new MarkdownIt().use(markdownItImageSize);
+
+    const imageUrl =
+      "//images.unsplash.com/photo-1577811037855-935237616bac?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2167&q=80";
+    const markdown = `![](${imageUrl})`;
+
+    const imageWidth = 2167;
+    const imageHeight = 1625;
+
+    const expected = `<p><img src="${imageUrl}" alt="" width="${imageWidth}" height="${imageHeight}"></p>\n`;
+    const actual = markdownRenderer.render(markdown);
+
+    expect(actual).toBe(expected);
+  });
 });
