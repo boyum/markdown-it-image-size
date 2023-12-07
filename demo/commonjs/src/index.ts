800ad0474543e3bed59f3dd2b19fca337448bd6f
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-12-07 12:41:49 +0100

    build: add `commonjs` example

---
+++ demo/commonjs/src/index.ts
@@ -0,0 +1,16 @@
+import MarkdownIt from "markdown-it";
+
+import("markdown-it-image-size").then(({ markdownItImageSize }) => {
+  const md = MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+  }).use(markdownItImageSize);
+
+  md.parse(
+    `# Hello World ✨
+
+![image](https://images.unsplash.com/photo-1577811037855-935237616bac?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2167&q=80 "title")`,
+    {},
+  );
+});
---
+++ demo/commonjs/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "markdown-it-image-size",
+      fileName: format => `markdown-it-image-size.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["markdown-it"],
+    },
+  },
+});
