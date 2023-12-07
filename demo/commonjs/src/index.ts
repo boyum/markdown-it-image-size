9618aabbe7add0beb16f65f14e533d008ad74216
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-12-07 12:49:08 +0100

    docs: commonjsify

--- demo/commonjs/src/index.ts
+++ demo/commonjs/src/index.ts
@@ -1,16 +1,15 @@
 import MarkdownIt from "markdown-it";
+import { markdownItImageSize } from "markdown-it-image-size";
 
-import("markdown-it-image-size").then(({ markdownItImageSize }) => {
-  const md = MarkdownIt({
-    html: true,
-    linkify: true,
-    typographer: true,
-  }).use(markdownItImageSize);
+const md = MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+}).use(markdownItImageSize);
 
-  md.parse(
-    `# Hello World ✨
+md.parse(
+  `# Hello World ✨
 
 ![image](https://images.unsplash.com/photo-1577811037855-935237616bac?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2167&q=80 "title")`,
-    {},
-  );
-});
+  {},
+);
