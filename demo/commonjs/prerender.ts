d90e5baec91cde75971187731d8c921962882a99
Sindre Bøyum <boyum@users.noreply.github.com>
2024-11-25 21:36:50 +0100

    style: use recommended settings for biome lint (#747)

--- demo/commonjs/prerender.ts
+++ demo/commonjs/prerender.ts
@@ -6,7 +6,7 @@ const toAbsolute = (p: string) => path.resolve(__dirname, p);
 const template = fs.readFileSync(toAbsolute("./index.html"), "utf-8");
 
 const appHtml = render();
-const html = template.replace(`<!--app-html-->`, appHtml);
+const html = template.replace("<!--app-html-->", appHtml);
 
-const filePath = `output/index.html`;
+const filePath = "output/index.html";
 fs.writeFileSync(toAbsolute(filePath), html);
